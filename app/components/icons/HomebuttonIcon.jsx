7cb39ae31843c6a6f2a1013cc1e132561524e5f1
MagnusDevelopr <sudhanshusharma103@gmail.com>
2024-02-01 20:34:18 +0530

    Fix stroke width in button icons

--- app/components/icons/HomebuttonIcon.jsx
+++ app/components/icons/HomebuttonIcon.jsx
@@ -9,7 +9,7 @@ const HomebuttonIcon = () => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   className="h-4 w-4"
