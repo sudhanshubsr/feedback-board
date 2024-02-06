9d5e6c0b89b278a00594057ed57b33e8a55a234f
MagnusDevelopr <sudhanshusharma103@gmail.com>
2024-02-07 04:18:46 +0530

    Update ESLint configuration and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,31 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: 'lh3.googleusercontent.com',
-          },
-          {
-              protocol: 'https',
-              hostname: 'devforum-uploads.s3.dualstack.us-east-2.amazonaws.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'avatars.githubusercontent.com'
-          },
-          {
-            protocol: 'https',
-            hostname: 'github.com'
-          },
-          {
-            protocol: 'https',
-            hostname: 'imagesprojects.s3.ap-south-1.amazonaws.com'
-          }
-
-
-      ]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "devforum-uploads.s3.dualstack.us-east-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "imagesprojects.s3.ap-south-1.amazonaws.com",
+      },
+    ],
   },
-}
+  eslint: {ignoreDuringBuilds: true},
+};
 
 module.exports = nextConfig;
