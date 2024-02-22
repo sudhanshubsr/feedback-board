9c45e3cfa9320814ba23b33eb4a5157546deb835
MagnusDevelopr <sudhanshusharma103@gmail.com>
2024-02-22 17:02:22 +0530

    refac: Remove console.log statement in newsletter route.js

--- app/api/newsletter/route.js
+++ app/api/newsletter/route.js
@@ -6,7 +6,6 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req) {
     const jsonbody = await req.json();
     const { email } = jsonbody;
-    console.log(email);
   try {
     const data = await resend.emails.send({
       from: 'VoxBoard <support@sudhanshu.site>',
